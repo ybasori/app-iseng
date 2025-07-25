b37c52b37e07de82e7a7a8ca7e471860f8515af9
YUSUF PC <ybasori@gmail.com>
2025-07-25 10:39:59 +0700

    fix

--- app/index.ts
+++ app/index.ts
@@ -1,10 +1,53 @@
-import express, { Request, Response, NextFunction } from 'express';
-import dotenv from 'dotenv';
+import express, { NextFunction, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+// import routes from "./routes";
+// import { expandRouter } from "./helper";
+import dotenv from "dotenv";
+import fs from 'fs';
+import https from 'https';
 
-dotenv.config(); // Optional for local
+dotenv.config();
 
 const app = express();
+
+app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser());
+app.use("/bulma", express.static("node_modules/bulma/css"));
+app.use("/fa", express.static("node_modules/@fortawesome/fontawesome-free"));
+app.use("/api", (req, _res, next) => {
+  const coloredMethod = `\x1b[32m[${req.method}]\x1b[0m`;
+  const queryStr = Object.entries(req.query)
+    .map(([key, value]) => {
+      if (Array.isArray(value)) {
+        return value
+          .map((v) => `${key}=${encodeURIComponent(v.toString())}`)
+          .join("&");
+      } else {
+        return `${key}=${encodeURIComponent(value?.toString() ?? "")}`;
+      }
+    })
+    .join("&");
+
+  const timestamp = new Date().toISOString();
+  console.log(
+    `[${timestamp}]${coloredMethod} ${req.path}${
+      queryStr ? "?" + queryStr : ""
+    }`
+  );
+  console.log(``);
+  next();
+});
+
+// expandRouter(routes).forEach((item) =>
+//   item.controller && item.method
+//     ? app[item.method](
+//         item.path,
+//         item.middleware ? item.middleware : [],
+//         item.controller
+//       )
+//     : null
+// );
 
 // Root route to avoid Railway 502
 app.get('/', (_req: Request, res: Response) => {
@@ -23,8 +66,20 @@ app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
   res.status(500).send('Internal Server Error');
 });
 
-// Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+
+if(process.env.NODE_ENV !== "production"){
+
+const key = fs.readFileSync('./ssl/key.pem');
+const cert = fs.readFileSync('./ssl/cert.pem');
+
+https.createServer({ key, cert }, app).listen(process.env.PORT, () => {
+  console.log(`server running on port ${process.env.PORT}`);
 });
+
+}
+else{
+  
+https.createServer(app).listen(process.env.PORT, () => {
+  console.log(`server running on port ${process.env.PORT}`);
+});
+}
