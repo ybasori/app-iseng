f9b864e2e5205bd7dbf952c048d5135c08743f46
YUSUF PC <ybasori@gmail.com>
2025-07-25 10:44:17 +0700

    fix

--- app/index.ts
+++ app/index.ts
@@ -10,34 +10,37 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
-app.use("/bulma", express.static("node_modules/bulma/css"));
-app.use("/fa", express.static("node_modules/@fortawesome/fontawesome-free"));
-app.use("/api", (req, _res, next) => {
-  const coloredMethod = `\x1b[32m[${req.method}]\x1b[0m`;
-  const queryStr = Object.entries(req.query)
-    .map(([key, value]) => {
-      if (Array.isArray(value)) {
-        return value
-          .map((v) => `${key}=${encodeURIComponent(v.toString())}`)
-          .join("&");
-      } else {
-        return `${key}=${encodeURIComponent(value?.toString() ?? "")}`;
-      }
-    })
-    .join("&");
 
-  const timestamp = new Date().toISOString();
-  console.log(
-    `[${timestamp}]${coloredMethod} ${req.path}${
-      queryStr ? "?" + queryStr : ""
-    }`
-  );
-  console.log(``);
-  next();
-});
+// app.use(express.static("public"));
+// app.use("/bulma", express.static("node_modules/bulma/css"));
+// app.use("/fa", express.static("node_modules/@fortawesome/fontawesome-free"));
+// app.use("/api", (req, _res, next) => {
+//   const coloredMethod = `\x1b[32m[${req.method}]\x1b[0m`;
+//   const queryStr = Object.entries(req.query)
+//     .map(([key, value]) => {
+//       if (Array.isArray(value)) {
+//         return value
+//           .map((v) => `${key}=${encodeURIComponent(v.toString())}`)
+//           .join("&");
+//       } else {
+//         return `${key}=${encodeURIComponent(value?.toString() ?? "")}`;
+//       }
+//     })
+//     .join("&");
+
+//   const timestamp = new Date().toISOString();
+//   console.log(
+//     `[${timestamp}]${coloredMethod} ${req.path}${
+//       queryStr ? "?" + queryStr : ""
+//     }`
+//   );
+//   console.log(``);
+//   next();
+// });
+
+
 
 // expandRouter(routes).forEach((item) =>
 //   item.controller && item.method
